17b112261f6bc4efad6b51eff0c905b54c753f1f
Takuma <takuma.x.ozeki@gmail.com>
2026-05-04 18:43:58 +0900

    fix app icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "English Shadowing",
   description: "Shadowing App",
+
+  icons: {
+    icon: "/icon.png",
+    apple: "/icon.png",
+  },
 };
 
 export default function RootLayout({
