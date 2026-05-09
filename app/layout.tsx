4e27b60a6e7bc8644cbad4ba1bd2650d828579c3
Takuma <takuma.x.ozeki@gmail.com>
2026-05-10 00:32:56 +0900

    fix tailwind ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,8 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "English Shadowing",
-  description: "Shadowing App",
-
-  icons: {
-    icon: "/icon.png",
-    apple: "/icon.png",
-  },
+  description: "English Shadowing App",
 };
 
 export default function RootLayout({
@@ -28,11 +11,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="ja"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="ja">
+      <body>{children}</body>
     </html>
   );
 }
